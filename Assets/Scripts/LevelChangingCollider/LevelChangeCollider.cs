3912ceccb67ed3c9869c3bd5fb6d40fd635ca4c9
Alodith <alodith15@gmail.com>
2022-04-26 13:49:04 +0100

    Main level begins, boss fight added. Water with reflections

---
+++ Assets/Scripts/LevelChangingCollider/LevelChangeCollider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangeCollider : MonoBehaviour
+{
+    
+
+    private void OnTriggerStay(Collider ChangeScene)
+    {
+        if (ChangeScene.gameObject.CompareTag("Player"))
+        {
+            print("player in collider");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        
+    }
+}
